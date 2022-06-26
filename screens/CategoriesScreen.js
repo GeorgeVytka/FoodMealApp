514315f374eab7e62d3b74473ae449753d822816
George <george.vytka@yahoo.com>
2022-06-26 17:01:17 -0400

    added navigation

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,31 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+import CategoryGridItem from "../compnents/CategoryGridTitle";
+//define func here to keep the code clean in the flat list
+
+function CategoriesScreen({ navigation }) {
+  function renderCategoryItem(itemData) {
+    function pressHandler() {
+      navigation.navigate("MealsOverview", {
+        categoryId: itemData.item.id,
+      });
+    }
+    return (
+      <CategoryGridItem
+        title={itemData.item.title}
+        color={itemData.item.color}
+        onPress={pressHandler}
+      />
+    );
+  }
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
