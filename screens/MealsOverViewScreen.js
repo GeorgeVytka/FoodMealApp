851e2e268aeb36cf416ccea9063cc743242f736d
George <george.vytka@yahoo.com>
2022-06-28 20:28:51 -0400

    created meals item page

--- screens/MealsOverViewScreen.js
+++ screens/MealsOverViewScreen.js
@@ -1,10 +1,22 @@
 import { MEALS } from "../data/dummy-data";
-import { View, Text, StyleSheet } from "react-native";
+import { View, FlatList, StyleSheet } from "react-native";
+import MealItem from "../compnents/MealItem";
 function MealsOverViewScreen({ navigation, route }) {
   const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
   return (
     <View style={styles.container}>
-      <Text>Meals - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
